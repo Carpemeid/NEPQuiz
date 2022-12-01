db76b06e9ecb7e5665ba0e548f70022d0caf5428
Dan Andoni <andonidan@gmail.com>
2022-12-01 10:15:16 +0200

    Initial Commit

---
+++ NEPQuiz/NEPQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEPQuizApp.swift
+//  NEPQuiz
+//
+//  Created by Dan Andoni on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NEPQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
